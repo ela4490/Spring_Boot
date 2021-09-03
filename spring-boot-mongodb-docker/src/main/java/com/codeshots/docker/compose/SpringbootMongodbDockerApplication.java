d35891de5f54690ffa1db352307cf43bfee3ff1d
ela4490 <ela4490@gmail.com>
2021-09-03 12:34:17 +0200

    Create spring-boot-mongodb-docker

---
+++ spring-boot-mongodb-docker/src/main/java/com/codeshots/docker/compose/SpringbootMongodbDockerApplication.java
@@ -0,0 +1,34 @@
+package com.codeshots.docker.compose;
+
+import com.codeshots.docker.compose.dao.BookRepository;
+import com.codeshots.docker.compose.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/book")
+public class SpringbootMongodbDockerApplication {
+
+    @Autowired
+    private BookRepository repository;
+
+    @PostMapping
+    public Book saveBook(@RequestBody Book book) {
+        return repository.save(book);
+    }
+
+    @GetMapping
+    public List<Book> getBooks() {
+        return repository.findAll();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongodbDockerApplication.class, args);
+    }
+
+}
